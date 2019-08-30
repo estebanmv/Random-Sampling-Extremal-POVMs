c54f68d971d0f42381172f073449f9e27776c34c
estebanmv <ozyorsk@gmail.com>
2019-08-30 11:38:15 +0200

    Guardo programa python que hace imágenes.

---
+++ figura2.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+DAnsatz = pd.read_csv('AnsatzDisplacedThermal.dat', header=None, sep='\t')
+DAncpmt = pd.read_csv('AnsatzDisplacedThermalcmpmt.dat',header=None, sep='\t')
+
+Dprueba = pd.read_csv('CC-DTherGaussian/DisplacedThermalSqrt-redo.dat', header=None, sep=',')
+Dprueba.columns=['x','y']
+Dprueba.x = Dprueba.x.astype(float)
+Dprueba.y = Dprueba.y.astype(float)
+
+#DAnsatz.columns=['x','y']
+df = DAncpmt.append(DAnsatz,ignore_index=True)
+df.columns=['x','y']
+
+plt.figure(figsize= (2.5,2))
+#plt.figure(figsize=(8,8))
+plt.plot(np.sqrt(Dprueba.x),Dprueba.y,'ro', markersize=2)
+plt.plot(np.sqrt(df.x[:29]),df.y[:29],'b-',linewidth=0.7)
+#plt.tick_params(labelsize=7,width=0.5)
+plt.tick_params(labelsize=5,width=0.5)
+plt.savefig('DisplacedThermal.pdf')
+#plt.show()
+
+CohAnsatz = pd.read_csv('coherent_Numerical_ansatz.dat', header=None, sep='\t')
+CohAnsatz.columns = ['x', 'y']
+CAlg = pd.read_csv('CC-Coherent/ExtremalCoherent-redo.dat', header=None, sep=',')
+CAlg.columns = ['x', 'y']
+
+plt.figure(figsize= (2.5,2))
+plt.plot(CohAnsatz.x.iloc[:86],CohAnsatz.y.iloc[:86], "b-",linewidth=0.7)
+plt.plot(np.sqrt(CAlg.x), CAlg.y, "ro", markersize=2)
+plt.tick_params(labelsize=5,width=0.5)
+#plt.xticks([0.,0.4,0.8,1.2])
+#plt.yticks([1.5,2.5,3.5,4.5,5.5])
+plt.savefig('Coherent.pdf')
